42406276e37fd2aa622569d9be67deb19f510749
anuLoveCoading <anusrivas07@gmail.com>
2023-03-25 07:16:45 +0530

    Pyhton_practise

---
+++ multitherading.py
@@ -0,0 +1,31 @@
+from threading import Thread
+from time import sleep
+
+class A(Thread):
+    def name(self):
+        for i in range(5):
+            print("Anurag")
+            sleep(2)
+            
+
+
+
+class B(Thread):
+    def name1(self):
+        for i in range(5):
+            print("Anurag Babu")
+            sleep(4)
+
+
+
+t1 = A()
+t2 = B()
+
+
+t1.start()
+t2.start()
+
+
+
+
+
