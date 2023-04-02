2285e59aceffed22662f0bf006860335782becb4
anuLoveCoading <anusrivas07@gmail.com>
2023-04-02 17:13:45 +0530

    python_practise

--- multitherading.py
+++ multitherading.py
@@ -2,17 +2,17 @@ from threading import Thread
 from time import sleep
 
 class A(Thread):
-    def name(self):
+    def run(self):
         for i in range(5):
             print("Anurag")
-            sleep(2)
+            sleep(1)
             
 
 class B(Thread):
-    def name1(self):
+    def run(self):
         for i in range(5):
             print("Anurag Babu")
-            sleep(4)
+            sleep(1)
 
 
 
@@ -22,6 +22,12 @@ t2 = B()
 
 t1.start()
 t2.start()
+
+t1.join()
+t2.join()
+
+
+print("Ankit")
 
 
 
