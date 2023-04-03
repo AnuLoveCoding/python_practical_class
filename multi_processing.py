b905ef2d9a837d9558a52784061899a6aacaa6a3
anuLoveCoading <anusrivas07@gmail.com>
2023-04-04 00:25:25 +0530

    pyhton_practise

--- multi_processing.py
+++ multi_processing.py
@@ -9,7 +9,8 @@ def calc_cube(numbers):
         print('cube' + str(n*n*n))
 
 if __name__== '__main__':
-    arr=[2,5,6,7]
+
+    arr=[2,5,6,7,8,9,7,8]
     p1= multiprocessing.Process(target= calc_square,args=(arr,))
     p2= multiprocessing.Process(target= calc_cube,args=(arr,))
 
